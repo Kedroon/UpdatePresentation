ec392c8979d319fdf65b14604a94ccf01451f183
Miguel Figueiredo Marques <miguelkedroon@gmail.com>
2016-05-20 05:12:30 -0400

    Second

--- UpdatePresentation/Program.cs
+++ UpdatePresentation/Program.cs
@@ -9,6 +9,7 @@ using System.Runtime.InteropServices;
 using Microsoft.Office.Core;
 using System.Net;
 using HtmlAgilityPack;
+using System.Threading;
 
 namespace UpdatePresentation
 {
@@ -59,7 +60,7 @@ namespace UpdatePresentation
 
 
             Excel.Application excelApp = new Excel.Application();
-            excelApp.Visible = true;
+            excelApp.Visible = false;
 
             string workbookPath = @"C:\Users\migue\Desktop\Cotacoes.xlsx";
             var workbooks = excelApp.Workbooks;
@@ -70,14 +71,14 @@ namespace UpdatePresentation
             string currentSheet = "Planilha1";
             //MessageBox.Show(currentSheet);
             Excel.Worksheet excelWorksheet = (Excel.Worksheet)excelSheets.get_Item(currentSheet);
-            excelWorksheet.Cells[5, 2] = dolarcompra.InnerHtml;
-            excelWorksheet.Cells[5, 3] = dolarvenda.InnerHtml;
+            excelWorksheet.Cells[5, 2] = dolarcompra.InnerHtml.Replace(",",".");
+            excelWorksheet.Cells[5, 3] = dolarvenda.InnerHtml.Replace(",", ".");
 
-            excelWorksheet.Cells[6, 2] = eurocompra.InnerHtml;
-            excelWorksheet.Cells[6, 3] = eurovenda.InnerHtml;
+            excelWorksheet.Cells[6, 2] = eurocompra.InnerHtml.Replace(",", ".");
+            excelWorksheet.Cells[6, 3] = eurovenda.InnerHtml.Replace(",", ".");
 
-            excelWorksheet.Cells[7, 2] = yencompra.InnerHtml;
-            excelWorksheet.Cells[7, 3] = yenvenda.InnerHtml;
+            excelWorksheet.Cells[7, 2] = yencompra.InnerHtml.Replace(",", ".");
+            excelWorksheet.Cells[7, 3] = yenvenda.InnerHtml.Replace(",", ".");
 
             excelWorkbook.Save();
             Marshal.ReleaseComObject(excelWorksheet);
@@ -96,6 +97,68 @@ namespace UpdatePresentation
             oPres.UpdateLinks();
             oPres.Save();
             oPres.SlideShowSettings.Run();
+
+
+            Thread.Sleep(2000);
+            updateexcel(1,2,3, 4,5,6);
+            oPres.UpdateLinks();
+            oPres.Save();
+            Thread.Sleep(2000);
+            updateexcel(7, 8, 9, 10, 11, 12);
+            oPres.UpdateLinks();
+            oPres.Save();
+            Thread.Sleep(2000);
+            updateexcel(1, 5, 4, 175, 134, 242);
+            oPres.UpdateLinks();
+            oPres.Save();
+            Thread.Sleep(2000);
+            updateexcel(134, 134, 242, 2142, 42, 424);
+            oPres.UpdateLinks();
+            oPres.Save();
+            Console.Read();
+            oPres.Close();
+            Marshal.ReleaseComObject(oPres);
+            Marshal.ReleaseComObject(oPresSet);
+            //Marshal.ReleaseComObject(ppApp);
+            ppApp.Quit();
+
+
         }
+
+        public static void updateexcel(Decimal num1, Decimal num2, Decimal num3 , Decimal num4, Decimal num5 , Decimal num6) {
+
+            Excel.Application excelApp = new Excel.Application();
+            excelApp.Visible = false;
+
+            string workbookPath = @"C:\Users\migue\Desktop\Cotacoes.xlsx";
+            var workbooks = excelApp.Workbooks;
+            Excel.Workbook excelWorkbook = workbooks.Open(workbookPath,
+                    0, false, 5, "", "", false, Excel.XlPlatform.xlWindows, "",
+                    true, false, 0, true, false, false);
+            Excel.Sheets excelSheets = excelWorkbook.Worksheets;
+            string currentSheet = "Planilha1";
+            //MessageBox.Show(currentSheet);
+            Excel.Worksheet excelWorksheet = (Excel.Worksheet)excelSheets.get_Item(currentSheet);
+            excelWorksheet.Cells[5, 2] = num1;
+            excelWorksheet.Cells[5, 3] = num2;
+
+            excelWorksheet.Cells[6, 2] = num3;
+            excelWorksheet.Cells[6, 3] = num4;
+
+            excelWorksheet.Cells[7, 2] = num5;
+            excelWorksheet.Cells[7, 3] = num6;
+
+            excelWorkbook.Save();
+            excelWorkbook.Close();
+            Marshal.ReleaseComObject(excelWorksheet);
+            Marshal.ReleaseComObject(excelSheets);
+            Marshal.ReleaseComObject(excelWorkbook);
+            Marshal.ReleaseComObject(workbooks);
+            //Marshal.ReleaseComObject(excelApp);
+            excelApp.Quit();
+
+        }
+
+
     }
 }
